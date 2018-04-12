1d7bc7b035949040a812a32d2a008d517f97b55f
yeahz <269386394@qq.com>
2018-04-12 10:55:25 +0800

    version2

---
+++ src/main/java/UserServiceImpl/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package UserServiceImpl;
+
+/**
+ * User: windows
+ * Date: 2018/4/12
+ * Time: 10:48
+ * Version:V1.0
+ */
+public class UserServiceImpl {
+    public static  void main(String[]args){
+
+    System.out.println("hello");
+
+
+    }
+}
